6a31578366d86a8d25e9ddb425dfdbbc0849fb92
Robrecht Cannoodt <rcannood@gmail.com>
2019-03-14 15:31:12 +0100

    update container

--- example.sh
+++ example.sh
@@ -3,7 +3,7 @@
 # generate dataset with certain seed
 set.seed(1)
 data <- dyntoy::generate_dataset(
-  id = "specific_example/template",
+  id = "specific_example/elpilinear",
   num_cells = 99,
   num_features = 101,
   model = "linear",
@@ -15,7 +15,6 @@ data$params <- list(
   NumNodes = 20L,
   MaxNumberOfIterations = 3
 )
-
 data$seed <- 1
 
 # write example dataset to file
